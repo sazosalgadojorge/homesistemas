568c2eed7125dd329ba9e2cc6b49e4653de94141
sazosalgadojorge <sazosalgadojorge@gmail.com>
2025-05-12 15:35:20 -0400

    Pre Final

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,12 +1,29 @@
 function renderhead() {
     fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('header-area').innerHTML = data;
-    })
-    .catch(error => console.error('Error cargando el header:', error));
-}
-
-renderhead();
-
-
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('header-area').innerHTML = data;
+  
+        const headerArea = document.getElementById('header-area');
+        const openButton = headerArea.querySelector(".th-menu-toggle.d-block");
+        const menuWrapper = document.querySelector(".th-menu-wrapper");
+        const closeButton = document.querySelector(".th-menu-wrapper .th-menu-toggle");
+  
+        if (openButton && menuWrapper) {
+          openButton.addEventListener("click", function () {
+            menuWrapper.classList.add("th-body-visible");
+            console.log("Menú mostrado");
+          });
+        }
+  
+        if (closeButton && menuWrapper) {
+          closeButton.addEventListener("click", function () {
+            menuWrapper.classList.remove("th-body-visible");
+            console.log("Menú ocultado");
+          });
+        }
+      })
+      .catch(error => console.error('Error cargando el header:', error));
+  }
+  
+  renderhead();
